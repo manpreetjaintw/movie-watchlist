d216cf8c4b09ad3d73ac3c93d17fcf5b82601d64
manpreetjaintw <manpreetjaintw@gmail.com>
2024-05-06 15:01:05 +0530

    temporary cors issue resolve

--- src/services/fetchDataFromApi.js
+++ src/services/fetchDataFromApi.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const apiUrl = 'http://www.omdbapi.com'  
+const apiUrl = 'https://www.omdbapi.com'
 const apiKey = "14c77f56"
 
 export const fetchDataFromApi = async (params) => {
   try {
-    const response = await axios.get( `${apiUrl}/?apikey=${apiKey}&s=${params}`);
+    const response = await axios.get(`${apiUrl}/?apikey=${apiKey}&s=${params}`);
     return response.data;
   } catch (error) {
     throw error;
